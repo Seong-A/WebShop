8c1623ec6e309cd1dd95d46b6879b695e5e35050
Seong-A <ksa6777@naver.com>
2024-01-05 17:13:48 +0900

    start

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.listen(5500, function(){
+    console.log('listening on 5000')
+});
+
+
+app.get('/item', function(request, require){
+    require.send('아이템을 보여주는 페이지입니다.');
+})
+
+app.get('/', function(request,require){
+    require.sendFile(__dirname + '/index.html')
+})
