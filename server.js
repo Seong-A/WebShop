01255165d75829ac6deec09b22d7978804816f0d
Seong-A <ksa6777@naver.com>
2024-01-11 17:38:48 +0900

    add html

--- server.js
+++ server.js
@@ -2,14 +2,9 @@ const express = require('express');
 const app = express();
 
 app.listen(5500, function(){
-    console.log('listening on 5500')
+    console.log('listening on 5500');
 });
 
-
-app.get('/item', function(request, require){
-    require.send('아이템을 보여주는 페이지입니다.');
-})
-
-app.get('/', function(request,require){
-    require.sendFile(__dirname + '/index.html')
-})
+app.get('/', function(request, response){  
+    response.sendFile(__dirname + '/index.html');
+});
